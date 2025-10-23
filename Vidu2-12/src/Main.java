4494b0729039e203a9650ff133ce07cbe8729e72
levananh2006 <02012006anhvanh@gmail.com>
2025-10-23 11:30:59 +0700

    Add files via upload

---
+++ Vidu2-12/src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Nhập một ký tự: ");
+        char ch = sc.next().charAt(0);
+
+        if (KiTu.isUpperCaseLetter(ch)) {
+            System.out.println("'" + ch + "' là chữ cái in hoa.");
+        } else {
+            System.out.println("'" + ch + "' không phải là chữ cái in hoa.");
+        }
+
+        sc.close();
+    }
+}
